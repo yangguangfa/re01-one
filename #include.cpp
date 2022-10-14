c9fcd9240b0fc29a55587c556634acc85cc0fd54
yangguangfa <115793028+yangguangfa@users.noreply.github.com>
2022-10-14 19:39:52 +0800

    Add files via upload

---
+++ #include.cpp
@@ -0,0 +1,19 @@
+#include  
+#include 
+#include 
+
+
+int main(int argc,char *argv[]){
+	float x,y,a;
+	 for(y=1.5;y>-1.5;y-=0.1){
+	 	for(x=-1.5;x<1.5;x+=0.05){
+	 		a=x*x+y*y-1;
+	 		putchar(a*a*a-x*x*y*y*y<=0.0?'*':' ');
+		 }
+		 system("color 0c");
+		 putchar('\n');
+	    }
+	 printf("王连艾\n");
+	 printf("永远开心快乐！\n");
+	 return 0;
+}
